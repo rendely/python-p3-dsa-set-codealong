1c7691bdd3b865e5d9e561910a6771af020fa421
matthew rendely <matthew.rendely@gmail.com>
2023-06-24 20:29:59 -0700

    Done

--- lib/MySet.py
+++ lib/MySet.py
@@ -1,3 +1,20 @@
 class MySet:
 
-    pass
+    def __init__(self, enumerable=[]):
+        self.dictionary = {}
+        for value in enumerable:
+            self.dictionary[value] = True
+    
+    def has(self, val):
+        return self.dictionary.get(val, False)
+
+    def add(self, val):
+        self.dictionary[val] = True
+        return self
+
+    def delete(self, val):
+        self.dictionary.pop(val, None)
+        return self
+
+    def size(self):
+        return len(self.dictionary)
